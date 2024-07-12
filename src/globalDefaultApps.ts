3218babc16d7176657ef38a072b8d5ef172e5f72
YaChengMu <1140049363@qq.com>
2024-07-12 16:11:07 +0800

    feat(TikTok): 局部广告-悬浮广告

---
+++ src/globalDefaultApps.ts
@@ -0,0 +1,52 @@
+import { batchImportApps } from '@gkd-kit/tools';
+
+const apps = await batchImportApps(`${import.meta.dirname}/apps`);
+
+// 全局规则黑名单
+// 在一些非系统应用中禁用所有全局规则
+export const blackListAppIDs: string[] = [];
+
+// 如果某应用的规则中已有全局规则中的某一类的规则, 则在此应用禁用对应全局规则
+function filterAppsByGroup(apps: any[], groupNamePrefix: string): string[] {
+  return apps
+    .filter(
+      (a) =>
+        a.groups.filter((g: { name: string }) =>
+          g.name.startsWith(groupNamePrefix),
+        ).length > 0,
+    )
+    .map((a) => a.id);
+}
+
+// 在应用中单独禁用某个全局规则
+// 开屏广告黑名单
+export const openAdBlackListAppIDs = new Set([
+  ...blackListAppIDs,
+  ...filterAppsByGroup(apps, '开屏广告'),
+]);
+
+// 更新提示黑名单
+export const updateBlackListAppIDs = new Set([
+  ...blackListAppIDs,
+  ...filterAppsByGroup(apps, '更新提示'),
+]);
+
+// 青少年模式黑名单
+export const yongBlackListAppIDs = new Set([
+  ...blackListAppIDs,
+  ...filterAppsByGroup(apps, '青少年模式'),
+]);
+
+// 全局规则白名单（由于系统应用默认禁用全局规则，所以对系统应用启用白名单模式）
+// 在一些系统软件中启用所有全局规则
+export const whiteListAppIDs: string[] = [];
+
+// 在应用中单独启用某个全局规则
+// 开屏广告白名单
+export const openAdWhiteListAppIDs = new Set([...whiteListAppIDs]);
+
+// 更新提示白名单
+export const updateWhiteListAppIDs = new Set([...whiteListAppIDs]);
+
+// 青少年模式白名单
+export const yongWhiteListAppIDs = new Set([...whiteListAppIDs]);
