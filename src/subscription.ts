6ad99533029f3f225880700b414c726629ea4fa4
YaChengMu <1140049363@qq.com>
2024-07-12 21:58:15 +0800

    feat(all): 更新订阅名称

--- src/subscription.ts
+++ src/subscription.ts
@@ -4,10 +4,10 @@ import categories from './categories';
 import globalGroups from './globalGroups';
 
 export default defineGkdSubscription({
-  id: 233,
-  name: 'Subscription',
+  id: 303,
+  name: 'YaChengMu的GKD订阅',
   version: 0,
-  author: 'author',
+  author: 'YaChengMu',
   checkUpdateUrl: './gkd.version.json5',
   supportUri: 'https://github.com/gkd-kit/subscription-template',
   categories,
