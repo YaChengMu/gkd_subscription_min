370225e6e99deecc8fa5a45b4fe7d51d9a73bf15
YaChengMu <1140049363@qq.com>
2025-02-05 15:44:44 +0800

    feat(all): 规则优化

--- src/globalDefaultApps.ts
+++ src/globalDefaultApps.ts
@@ -11,8 +11,10 @@ export const blackListAppIDs: string[] = [
   'com.eg.android.AlipayGphone', //支付宝
   'com.tencent.wetype', // 微信输入法
   'com.tmri.app.main', // 交管12123
+  'org.telegram.messenger', // Telegram
   'com.accessibilitymanager', // 无障碍管理器
   'com.alicloud.databox', // 阿里云盘
+  'com.quark.browser', // 夸克浏览器 开屏广告误触 https://i.gkd.li/i/18221460
 ];
 
 // 如果某应用的规则中已有全局规则中的某一类的规则, 则在此应用禁用对应全局规则
@@ -31,18 +33,29 @@ function filterAppsByGroup(apps: RawApp[], groupNamePrefix: string): string[] {
 // 开屏广告黑名单
 export const openAdBlackListAppIDs = new Set([
   ...blackListAppIDs,
+  'com.sankuai.meituan', // 美团 误触 https://i.gkd.li/i/17827264
   ...filterAppsByGroup(apps, '开屏广告'),
 ]);
 
 // 更新提示黑名单
 export const updateBlackListAppIDs = new Set([
   ...blackListAppIDs,
+  'com.jingdong.app.mall', // 京东
   ...filterAppsByGroup(apps, '更新提示'),
 ]);
 
 // 青少年模式黑名单
 export const yongBlackListAppIDs = new Set([
   ...blackListAppIDs,
+  'com.autonavi.minimap', // 高德地图
+  'com.MobileTicket', // 铁路12306
+  'com.sinovatech.unicom.ui', // 中国联通
+  'com.sankuai.meituan', // 美团
+  'com.taobao.idlefish', // 闲鱼
+  'com.tencent.mobileqq', // QQ
+  'com.taobao.taobao', // 淘宝
+  'com.ct.client', // 中国电信
+  'me.ele', // 饿了么
   ...filterAppsByGroup(apps, '青少年模式'),
 ]);
 
