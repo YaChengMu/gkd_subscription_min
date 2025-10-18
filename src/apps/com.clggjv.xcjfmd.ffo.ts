b6dbb83e89920ce0a2b5da8237eaf83f6cece1d1
YaChengMu <1140049363@qq.com>
2025-10-18 09:56:51 +0800

    docs(readme): 更新版本号至v84
    - 更新README中的当前版本号
    - 更新dist目录下的变更日志和版本说明
    - 添加新的全局规则和应用规则条目
    - 调整规则分类表格结构

--- src/apps/com.clggjv.xcjfmd.ffo.ts
+++ src/apps/com.clggjv.xcjfmd.ffo.ts
@@ -17,18 +17,17 @@ export default defineGkdApp({
         {
           key: 1,
           activityIds: 'com.clggjv.xcjfmd.ffo.MainActivity',
-          action: 'clickCenter',
           matches: [
             '@ImageView < FrameLayout <2 FrameLayout <2 FrameLayout < FrameLayout < [id="android:id/content"]',
-            '@ImageView < FrameLayout <3 FrameLayout < FrameLayout <2 FrameLayout < FrameLayout < [id="android:id/content"]',
-            '@ImageView < FrameLayout <5 FrameLayout <2 FrameLayout < FrameLayout < [id="android:id/content"]',
             '@ImageView < FrameLayout <3 FrameLayout <2 FrameLayout < FrameLayout < [id="android:id/content"]',
+            '@ImageView < FrameLayout <5 FrameLayout <2 FrameLayout < FrameLayout < [id="android:id/content"]',
+            '@ImageView < FrameLayout <3 FrameLayout < FrameLayout <2 FrameLayout < FrameLayout < [id="android:id/content"]',
           ],
           snapshotUrls: [
             'https://i.gkd.li/i/22875694',
-            'https://i.gkd.li/i/22920195',
-            'https://i.gkd.li/i/22927729',
             'https://i.gkd.li/i/22962861',
+            'https://i.gkd.li/i/22927729',
+            'https://i.gkd.li/i/22920195',
           ],
         },
         {
